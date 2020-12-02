fd9c41aec45fdfa47a8d18ee0fe181cc4d048c85
pipegris <andfeg@gmail.com>
2020-12-02 16:32:58 -0500

    Endpoints for device

---
+++ app/Models/DeviceData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DeviceData extends Model
+{
+
+	const UPDATED_AT = null;
+
+
+    protected $table = 'device_data';
+
+    protected $fillable = [
+    	'tem1',
+    	'tem2',
+    	'tem3',
+    	'tem4',
+    	'hum1',
+    	'hum2',
+    	'hum3',
+        'light1',
+        'light2',
+        'light3',
+        'light4',
+    	'ph1',
+    ];
+
+    protected $appends = ["tem_avg", "hum_avg", "light_avg"];
+
+
+    public function getTemperature() {
+        return $this->get(['tem1', 'tem2', 'tem3', 'tem4', 'created_at'], 'tem_avg');
+    }
+
+    public function getHumidity() {
+        return $this->get(['hum1', 'hum2', 'hum3', 'created_at'], 'hum_avg');
+    }
+
+    public function getLighting() {
+        return $this->get(['light1', 'light2', 'light3', 'light4', 'created_at'], 'light_avg');
+    }
+
+    public function getTemAvgAttribute() {
+        return ($this->tem1 + $this->tem2 + $this->tem3 + $this->tem4) / 4;
+    }
+
+    public function getHumAvgAttribute() {
+        return ($this->hum1 + $this->hum2 + $this->hum3) / 3;
+    }
+
+    public function getLightAvgAttribute() {
+        return ($this->light1 + $this->light2 + $this->light3 + $this->light4) / 4;
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 
+use App\Models\DeviceData;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,3 +20,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+
+//
+Route::get('/device/{device_id}/data', function (Request $request, $deviceId) {
+
+    $data = new DeviceData($request->all());
+    $data->save();
+    return $data->toArray();
+});
+
+
+Route::get('/device/{device_id}/temperature', function (Request $request, $deviceId) {
+
+    return (new DeviceData)->getTemperature();
+});
+
+Route::get('/device/{device_id}/humidity', function (Request $request, $deviceId) {
+
+	return (new DeviceData)->getHumidity();
+});
+
+Route::get('/device/{device_id}/light', function (Request $request, $deviceId) {
+
+	return (new DeviceData)->getLighting();
+});
+
+Route::get('/device/{device_id}/ph', function (Request $request, $deviceId) {
+
+    return DeviceData::select('ph1', 'created_at')->get();
+});
